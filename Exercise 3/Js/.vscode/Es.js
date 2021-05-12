198e73926cde3838083a920261c06a2881b48b80
Rismia <mariopoire@gmail.com>
2021-05-12 00:20:46 -0500

    Update Es.js

--- Exercise 3/Js/.vscode/Es.js
+++ Exercise 3/Js/.vscode/Es.js
@@ -1,40 +1,38 @@
 // change everything below to the newer Javascript!
 
 // let + const
-// Destructuring
-class person{
-  constructor( firstName, lastName, age, eyeColor, city ){
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.eyeColor = eyeColor;
-    this.city = city;
-  }
-}
-
-myperson = new person( "Mario", "Poire", 27, "Green", "mission" )
-
-// Object properties
 let a = 'test';
-let b = true;
-let c = 789;
+const b = true;
+const c = 789;
 a = 'test2';
 
-const okObj = {
-  a: a,
-  b: b,
-  c: c
+// Destructuring
+const person = {
+  firstName : "John",
+  lastName  : "Doe",
+  age       : 50,
+  eyeColor  : "blue",
+  city      : "Houston"
 };
+
+const { firstname, lastname, age, eyeColor, city } = person;
+
+// Object properties
+
+const okObj = {a:'a', b: 'b', c: 'c'}
 
 console.log(Object.keys(okObj));
 
 // Template strings
-console.log('Hello ' + myperson.firstName + ' have I met you before? I think we met in ' + myperson.city + ' last summer no???');
+let greeting = `Hello ${person.firstName} have I met you before? I think we met in ${person.city} last summer no???`
+console.log(greeting);
 
 // default arguments
 // default age to 10;
-let isValidAge = (age) => 10
-console.log(isValidAge())
+function isValidAge(age = 10) {
+  return age
+}
+console.log(isValidAge());
 
 // Symbol
 // Create a symbol: "This is my first Symbol"
